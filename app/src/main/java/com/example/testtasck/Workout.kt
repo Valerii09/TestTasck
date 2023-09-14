1c477a1853dc78bdec6e2e41eaa5c821479b4fc8
Valerii09 <valiera.trufanov.2001@gmail.com>
2023-09-14 23:34:39 +0700

    добавление иконки и изменение имен пакетов

--- app/src/main/java/com/example/testtasck/Workout.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testtasck
-
-import java.io.Serializable
-
-data class Workout(val date: String, val exercise: String, val duration: String) : Serializable
-val workout1 = Workout("2023-09-10", "squats", "30 minutes")
-val workout2 = Workout("2023-09-11", "Push ups", "20 minutes")
